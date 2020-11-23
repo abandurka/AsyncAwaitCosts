379d67f759fb3761dac69f000535cdd8a6e0f69b
Anton Bandurka <anton.bandurka@infobip.com>
2020-11-23 14:59:44 +0300

    added benchmark

---
+++ AsyncAwaitCosts.ConsoleAppBenchmark/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+namespace AsyncAwaitCosts.ConsoleAppBenchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<BenchmarkJob>();
+        }
+    }
+
+    [SimpleJob(RuntimeMoniker.NetCoreApp31)]
+    [RPlotExporter, MarkdownExporter]
+    [MemoryDiagnoser]
+    public class BenchmarkJob
+    {
+        [Params(20)]
+        public int N;
+        
+        private readonly double salt = new Random().NextDouble(); 
+        private IImmutableList<int> _inputData;
+        
+        [GlobalSetup]
+        public void Setup()
+        {
+            _inputData = Enumerable.Range(1, N).ToImmutableList();
+        }
+        
+        [Benchmark]
+        public async Task<double> ReturnAwaitedTask()
+        {
+            var summ = 0d;
+            for (var index = 0; index < _inputData.Count; index++)
+            {
+                var i = _inputData[index];
+                summ += await CalculateAwaitTaskAsync(i);
+            }
+
+            return summ;
+        }
+
+        [Benchmark]
+        public async Task<double> ReturnDoubleAwaitedTask()
+        {
+            var summ = 0d;
+            for (var index = 0; index < _inputData.Count; index++)
+            {
+                var i = _inputData[index];
+                summ += await CalculateDoubleAwaitedTaskAsync(i);
+            }
+
+            return summ;
+        }
+                
+        [Benchmark]
+        public async Task<double> ReturnDoubleTaskWithoutAwait()
+        {
+            var summ = 0d;
+            for (var index = 0; index < _inputData.Count; index++)
+            {
+                var i = _inputData[index];
+                summ += await CalculateDoubleNotAwaitedTaskAsync(i);
+            }
+
+            return summ;
+        }
+
+        [Benchmark]
+        public async Task<double> ReturnTaskWithoutAwait()
+        {
+            var summ = 0d;
+            for (var index = 0; index < _inputData.Count; index++)
+            {
+                var i = _inputData[index];
+                summ += await CalculateNotAwaitedTaskAsync(i);
+            }
+
+            return summ;
+        }
+        
+        [Benchmark]
+        public async Task<double> ReturnTaskFromResult()
+        {
+            var summ = 0d;
+            for (var index = 0; index < _inputData.Count; index++)
+            {
+                var i = _inputData[index];
+                summ += await CalculateTaskFromResultAsync(i);
+            }
+
+            return summ;
+        }
+        
+        [Benchmark]
+        public async Task<double> ReturnResultWithAsyncWord()
+        {
+            var summ = 0d;
+            for (var index = 0; index < _inputData.Count; index++)
+            {
+                var i = _inputData[index];
+                summ += await CalculateAsyncResultWithoutTaskAsync(i);
+            }
+
+            return summ;
+        }
+        
+        [Benchmark(Baseline = true)]
+        public double For()
+        {
+            var summ = 0d;
+            for (var index = 0; index < _inputData.Count; index++)
+            {
+                var i = _inputData[index];
+                summ += Salt(i);
+            }
+
+            return summ;
+        }
+        
+        private async Task<double> CalculateDoubleAwaitedTaskAsync(int i)
+        {
+            return await CalculateAwaitTaskAsync(i);
+        }
+        
+        private async Task<double> CalculateAwaitTaskAsync(int i)
+        {
+            return await Task.Run(() => Salt(i));
+        }
+        
+        private Task<double> CalculateDoubleNotAwaitedTaskAsync(int i)
+        {
+            return CalculateNotAwaitedTaskAsync(i);
+        }
+        
+        private Task<double> CalculateNotAwaitedTaskAsync(int i)
+        {
+            return Task.Run(() => Salt(i));
+        }
+        
+        private Task<double> CalculateTaskFromResultAsync(int i)
+        {
+            return Task.FromResult(Salt(i));
+        }
+        
+        private async Task<double> CalculateAsyncResultWithoutTaskAsync(int i)
+        {
+            return Salt(i);
+        }
+
+        double Salt(int i)
+        {
+            return i * salt;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # AsyncAwaitCosts
+
+``` ini
+
+BenchmarkDotNet=v0.12.1, OS=Windows 10.0.17763.1577 (1809/October2018Update/Redstone5)
+Intel Core i7-8550U CPU 1.80GHz (Kaby Lake R), 1 CPU, 8 logical and 4 physical cores
+.NET Core SDK=3.1.302
+  [Host]        : .NET Core 3.1.8 (CoreCLR 4.700.20.41105, CoreFX 4.700.20.41903), X64 RyuJIT
+  .NET Core 3.1 : .NET Core 3.1.8 (CoreCLR 4.700.20.41105, CoreFX 4.700.20.41903), X64 RyuJIT
+
+Job=.NET Core 3.1  Runtime=.NET Core 3.1  
+
+```
+|                       Method |  N |        Mean |       Error |      StdDev |  Ratio | RatioSD |  Gen 0 | Gen 1 | Gen 2 | Allocated |
+|----------------------------- |--- |------------:|------------:|------------:|-------:|--------:|-------:|------:|------:|----------:|
+|      ReturnDoubleAwaitedTask | 20 | 71,350.3 ns | 1,405.54 ns | 1,503.91 ns | 311.01 |    7.95 | 1.9531 |     - |     - |    8325 B |
+|            ReturnAwaitedTask | 20 | 50,544.7 ns |   996.51 ns |   932.14 ns | 219.61 |    7.84 | 1.4038 |     - |     - |    5887 B |
+| ReturnDoubleTaskWithoutAwait | 20 | 37,880.2 ns |   686.51 ns |   608.58 ns | 165.05 |    5.24 | 0.7935 |     - |     - |    3490 B |
+|       ReturnTaskWithoutAwait | 20 | 38,114.5 ns |   517.03 ns |   458.34 ns | 166.06 |    4.41 | 0.7935 |     - |     - |    3491 B |
+|         ReturnTaskFromResult | 20 |    593.5 ns |    14.37 ns |    40.52 ns |   2.74 |    0.17 | 0.3614 |     - |     - |    1512 B |
+|    ReturnResultWithAsyncWord | 20 |    926.8 ns |    20.65 ns |    57.22 ns |   4.16 |    0.29 | 0.3614 |     - |     - |    1512 B |
+|                          For | 20 |    228.9 ns |     4.46 ns |     6.10 ns |   1.00 |    0.00 |      - |     - |     - |         - |
